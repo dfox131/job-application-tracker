b80d0828704b1f3f1282ba6102e6fb88fed4dbd6
Daniel Fox <df99571n@pace.edu>
2026-03-17 21:42:35 -0400

    Fix Prisma client import for Render deployment

--- server/src/prisma.js
+++ server/src/prisma.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import prismaPkg from "@prisma/client";
+
+const { PrismaClient } = prismaPkg;
 
 const connectionString = process.env.DATABASE_URL;
 
